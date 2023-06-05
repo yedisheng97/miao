2ad7065b6bf20c5ca4fcae5233504af86181a5e1
yedisheng97 <18225723988@163.com>
2023-06-05 18:00:42 +0800

    作业lodash

--- lodash/yedisheng97-lodash.js
+++ lodash/yedisheng97-lodash.js
@@ -1,4 +1,4 @@
-let yedisheng97 = {
+var yedisheng97 = {
   chunk: function (array, size) {
     let result = []
     for (i = 0; i < array.length; i += size) {
@@ -28,20 +28,16 @@ let yedisheng97 = {
   differenceBy: function (array, ...values) {
     const value = values.flat()
     const iteratee = value[value.length - 1]
-    if(Array.isArray(iteratee)) {
+    if (Array.isArray(iteratee)) {
       return array.difference(array, ...values)
     }
-     if(typeof iteratee === "string") {
-       return array.filter(it => ! new Set(value.map(item => item[iteratee])).has(it[iteratee]))
-     }
-     if(typeof iteratee === "function") {
-       return array.filter(it => ! new Set(value.map(iteratee)).has(iteratee(it)))
-     }
-   },
-
-  //  differenceWith:function(array, other, comparator) {
-  //   return array.filter(arrVal => !other.some(othVal => comparator(arrVal, othVal)))
-  //  },
+    if (typeof iteratee === "string") {
+      return array.filter(it => !new Set(value.map(item => item[iteratee])).has(it[iteratee]))
+    }
+    if (typeof iteratee === "function") {
+      return array.filter(it => !new Set(value.map(iteratee)).has(iteratee(it)))
+    }
+  },
 
   drop: function (array, len = 1) {
     return array.slice(len)
@@ -51,11 +47,8 @@ let yedisheng97 = {
     return n > array.length ? [] : array.slice(0, array.length - n)
   },
 
-
-
   fill: function (array, start = 0, end = array.length) {
     return array.fill(value, start, end)
   },
-
 
 }
